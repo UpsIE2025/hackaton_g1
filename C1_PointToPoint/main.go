7e72cc3f96f6e027da9ee316ee4a25ba604b43fc
FPabl0 <f.pablo1@hotmail.com>
2025-02-22 10:21:42 -0500

    added template

--- C1_PointToPoint/main.go
+++ C1_PointToPoint/main.go
@@ -1,1 +1,31 @@
-para empezar a rpoibar
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+	})
+	defer rdb.Close()
+
+	r := gin.Default()
+
+	r.POST("/publish", func(ctx *gin.Context) {
+		var req struct {
+			Message string `json:"message"`
+		}
+		if err := ctx.ShouldBindBodyWithJSON(&req); err != nil {
+			ctx.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		ctx.Status(http.StatusOK)
+	})
+
+	r.Run(":80")
+}
